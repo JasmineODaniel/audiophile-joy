bbbad475281d1d31d4333153ddcf2e321f5341fb
THENEVERDONE <163352801+JasmineODaniel@users.noreply.github.com>
2025-12-31 05:42:48 -0800

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
-import { CategoryCard } from "@/components/CategoryCard";
+import { CategoryCard } from "@/components/CategoryCard"; 
 import { Button } from "@/components/ui/button";
 
 const Index = () => {
